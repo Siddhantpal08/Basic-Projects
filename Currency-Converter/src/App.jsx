df0a8ced4748bb58e4fbcb4170146f2703acd7ee
Siddhant Pal <palsiddhant3@gmail.com>
2025-01-27 22:41:28 +0530

    new project started

---
+++ Currency-Converter/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+const BASE_URL="";
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl bg-orange-500">Currency Converter</h1>
+      
+    </>
+  )
+}
+
+export default App
