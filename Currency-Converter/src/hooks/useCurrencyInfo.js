d1c034dfa9efb44a80577a165d1e7a484b0e0d27
Siddhant Pal <palsiddhant3@gmail.com>
2025-01-28 01:21:09 +0530

    `Added currency conversion functionality and refactored code to include input box component and useCurrencyInfo hook`

--- Currency-Converter/src/hooks/useCurrencyInfo.js
+++ Currency-Converter/src/hooks/useCurrencyInfo.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo (currency) {
+    const lowerCurrency = currency.toLowerCase();
     const [data, setData] = useState({})
     useEffect(() =>{
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.1.27/v1/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.1.27/v1/currencies/${lowerCurrency}.json`)
         .then((response) => response.json())
-        .then((res) => setData(res[currency]))
-    },[currency])
-    return data;
+        .then((response) => setData(response[lowerCurrency]))
+    },[lowerCurrency])
+    return data
 }
 
 export default useCurrencyInfo;
