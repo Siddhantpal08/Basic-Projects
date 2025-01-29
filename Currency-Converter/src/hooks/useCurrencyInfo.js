0c289130fbacf24ee0c18f06a44b3f771343b4f5
Siddhant Pal <palsiddhant3@gmail.com>
2025-01-29 15:51:19 +0530

    Final Changes

--- Currency-Converter/src/hooks/useCurrencyInfo.js
+++ Currency-Converter/src/hooks/useCurrencyInfo.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo (currency) {
-    const lowerCurrency = currency.toLowerCase();
-    const [data, setData] = useState({})
-    useEffect(() =>{
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.1.27/v1/currencies/${lowerCurrency}.json`)
-        .then((response) => response.json())
-        .then((response) => setData(response[lowerCurrency]))
-    },[lowerCurrency])
-    return data
+function useCurrencyInfo(currency) {
+  const lowerCurrency = currency.toLowerCase();
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.1.27/v1/currencies/${lowerCurrency}.json`)
+      .then((response) => response.json())
+      .then((response) => setData(response[lowerCurrency] || {}))
+      .catch(() => setData({}));
+  }, [lowerCurrency]);
+
+  return data;
 }
 
 export default useCurrencyInfo;
